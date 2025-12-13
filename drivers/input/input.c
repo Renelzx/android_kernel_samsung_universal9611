7e11a9b9b733da18a9c238f13f17114471ca501d
Ashley Hwang <ashstone-dev@protonmail.com>
2025-12-12 23:32:35 -0300

    Revert "KernelSU: Add minimized manual hooks 1.4"
    This reverts commit 315b15dc0d8656100fbf24d78d203caab8c639cd.

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -844,12 +844,6 @@ void input_booster_init(void)
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
-
-#ifdef CONFIG_KSU
-extern bool ksu_input_hook __read_mostly;
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-#endif
-
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
@@ -858,10 +852,6 @@ void input_event(struct input_dev *dev,
 	// Input Booster +
 	int idx = 0;
 
-#ifdef CONFIG_KSU
-	if (unlikely(ksu_input_hook))
-		ksu_handle_input_handle_event(&type, &code, &value);
-#endif
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
 		spin_lock_irqsave(&dev->event_lock, flags);
